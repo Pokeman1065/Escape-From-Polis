149349dec38da4673b31169a03bc96c89e2a9268
Noob <chincameron1@gmail.com>
2022-09-17 16:40:45 -0400

    Movement3

--- Fished/Assets/playMovement.cs
+++ Fished/Assets/playMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,13 @@ public class playMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public Transform cam;
-    public float moveSpeed = 15f;
-    public float turnSmoothTime = 0.5f;
+    public float moveSpeed = 10f;
+    public float turnSmoothTime = 0.2f;
 
     float turnSmoothVel;
     float forwardMove = 0;
     float sideMove = 0;
+    float targetAngle = 0;
     Vector3 dir;
     bool canJump = false;
 
@@ -23,6 +25,7 @@ public class playMovement : MonoBehaviour
     {
         forwardMove = Input.GetAxisRaw("Vertical");
         sideMove = Input.GetAxisRaw("Horizontal");
+        //check for jump allowed and jump
         if (!canJump && transform.position.y <= 1.1)
             canJump = true;
         if (Input.GetKeyDown("space") && canJump)
@@ -30,19 +33,22 @@ public class playMovement : MonoBehaviour
             rb.velocity = new Vector3(rb.velocity.x, 10f, rb.velocity.z);
             canJump = false;
         }
-            
+
         dir = new Vector3(sideMove, 0f, forwardMove).normalized;
     }
 
     private void FixedUpdate()
     {
-        float targetAngle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
-        float angle  = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
+        //check for new dir
+        if (forwardMove != 0 || sideMove != 0)
+            targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        //smooth rotation of player
+        float transAngle  = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, transAngle, 0f);
+        //move player
         Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-        rb.velocity = new Vector3(forwardMove * moveDir.x * moveSpeed, rb.velocity.y, forwardMove * moveDir.y * moveSpeed);
+        if (forwardMove != 0 || sideMove != 0)
+            rb.velocity = new Vector3(moveDir.x * moveSpeed, rb.velocity.y, moveDir.z * moveSpeed);
 
-        print(canJump);
     }
 }
