08cb79b65510ee15ec0bb325b2ddbcdd936cd62f
Noob <chincameron1@gmail.com>
2022-09-17 14:53:36 -0400

    Movement2

--- Fished/Assets/playMovement.cs
+++ Fished/Assets/playMovement.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class playMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float moveSpeed = 15;
+    public Transform cam;
+    public float moveSpeed = 15f;
+    public float turnSmoothTime = 0.5f;
 
+    float turnSmoothVel;
     float forwardMove = 0;
     float sideMove = 0;
+    Vector3 dir;
+    bool canJump = false;
 
     void Start()
     {
-        
     }
 
     void Update()
     {
         forwardMove = Input.GetAxisRaw("Vertical");
         sideMove = Input.GetAxisRaw("Horizontal");
+        if (!canJump && transform.position.y <= 1.1)
+            canJump = true;
+        if (Input.GetKeyDown("space") && canJump)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 10f, rb.velocity.z);
+            canJump = false;
+        }
+            
+        dir = new Vector3(sideMove, 0f, forwardMove).normalized;
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(sideMove * moveSpeed * 0.4f, rb.velocity.y, forwardMove * moveSpeed);
+        float targetAngle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle  = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        rb.velocity = new Vector3(forwardMove * moveDir.x * moveSpeed, rb.velocity.y, forwardMove * moveDir.y * moveSpeed);
+
+        print(canJump);
     }
 }
